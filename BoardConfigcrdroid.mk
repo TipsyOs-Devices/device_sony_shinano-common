a192087bc89a252dfb72e7002938db1ae8777b5c
Alx31 <alex31.couderc@gmail.com>
2015-12-06 19:37:19 +0100

    tipsy bringup+ twrp needs
    Change-Id: I6ce70d019b2dbf9966422aafa5961c06a9baee82

--- BoardConfigcrdroid.mk
+++ BoardConfigcrdroid.mk
@@ -15,8 +15,8 @@
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
 
-# crDroid SELinux
--include vendor/crdroid/sepolicy/qcom/sepolicy.mk
+# tipsy SELinux
+-include vendor/tipsy/sepolicy/qcom/sepolicy.mk
 
 # Kernel
 BOARD_CUSTOM_BOOTIMG := true
@@ -31,3 +31,30 @@ BOARD_NFC_CHIPSET := pn547
 BOARD_RECOVERY_SWIPE := true
 TARGET_NO_SEPARATE_RECOVERY := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+
+# TWRP Recovery
+TARGET_NO_SEPARATE_RECOVERY := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+DEVICE_RESOLUTION := 1080x1920
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_HAS_NO_RECOVERY_PARTITION := true
+TW_FLASH_FROM_STORAGE := true
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_DEFAULT_EXTERNAL_STORAGE := true
+TW_INCLUDE_JB_CRYPTO := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/msm_sdcc.1/by-name/userdata"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,barrier=1,noauto_da_alloc,discard"
+TW_CRYPTO_FS_FLAGS := "0x00000406"
+TW_CRYPTO_KEY_LOC := "footer"
+TW_INCLUDE_FUSE_EXFAT := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/wled:backlight/brightness
+TW_MAX_BRIGHTNESS := 255
+TW_NO_USB_STORAGE := true
+TW_NO_SCREEN_BLANK := true
+
